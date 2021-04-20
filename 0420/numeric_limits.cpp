cc76fa627f393415713ac8c98f746f1769b839ac
Ryo Kajiwara <sylph01@s01.ninja>
2021-04-20 22:18:23 +0900

    [0420] ezoe-book: numeric limits of float

--- 0420/numeric_limits.cpp
+++ 0420/numeric_limits.cpp
@@ -22,4 +22,28 @@ int main(){
     int int_max_plus_one  = int_max + 1; // in 8bit, 0b10000000 = int_min
 
     std::cout << int_min << "\n" << int_max << "\n" << int_min_minus_one << "\n" << int_max_plus_one << "\n";
+
+    std::cout << "----------\n";
+
+    std::cout
+      << "digits10"      << "\n"
+      << "float: "       << std::numeric_limits<float>::digits10       << "\n"
+      << "double: "      << std::numeric_limits<double>::digits10      << "\n"
+      << "long double: " << std::numeric_limits<long double>::digits10 << "\n" ;
+
+    std::cout << "----------\n";
+
+    std::cout
+      << "max_digits10"      << "\n"
+      << "float: "           << std::numeric_limits<float>::max_digits10       << "\n"
+      << "double: "          << std::numeric_limits<double>::max_digits10      << "\n"
+      << "long double: "     << std::numeric_limits<long double>::max_digits10 << "\n" ;
+
+    std::cout << "----------\n";
+
+    std::cout
+      << "epsilon"       << "\n"
+      << "float: "       << std::numeric_limits<float>::epsilon()       << "\n"
+      << "double: "      << std::numeric_limits<double>::epsilon()      << "\n"
+      << "long double: " << std::numeric_limits<long double>::epsilon() << "\n" ;
 }
