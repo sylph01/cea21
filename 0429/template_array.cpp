d2b582cf15d473f1add14fb51a728d39b07101d1
Ryo Kajiwara <sylph01@s01.ninja>
2021-04-29 22:34:59 +0900

    [0429] ezoe-book: const member functions, const references
    this is starting to get hellish

--- 0429/template_array.cpp
+++ 0429/template_array.cpp
@@ -4,25 +4,48 @@ template <typename T, std::size_t N>
 struct array{
   using value_type = T;
   using reference = T &;
+  using const_reference = T const &;
   using size_type = std::size_t;
 
+  size_type size() const {
+    return N;
+  }
+
   value_type storage[N];
+  // this returns a modifiable reference
   reference operator[](size_type i){
+    return storage[i];
+  }
+  // this returns a "const reference" T const &
+  const_reference operator[](size_type i) const {
     return storage[i];
   }
 };
 
+template <typename Array>
+// as this takes a const reference...
+void print(Array const & c){
+  // c.size() calls the const member function
+  for(std::size_t i = 0; i != c.size(); ++i){
+    // this calls the const_reference const member function
+    std::cout << c[i] << "\n";
+  }
+}
+
 int main(){
   auto p = [](auto x){ std::cout << x << "\n"; };
+  /*
   auto p_all = [=](auto x){
     for(auto iter = std::begin(x.storage); iter != std::end(x.storage); ++iter){
       p(*iter);
     }
   };
+  */
 
   array<int, 3> a3 = {1,2,3};
   array<double, 5> d5 = {0.1, 0.2, 0.3, 0.4, 0.5};
 
   p(a3[2]);
-  p_all(d5);
+  d5[2] = 1.3;
+  print(d5);
 }
