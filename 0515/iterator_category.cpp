dccc37f2383a985f90c4b97d953c092cc8412275
Ryo Kajiwara <sylph01@s01.ninja>
2021-05-15 16:33:30 +0900

    [0515] ezoe-book: iterator category, cont'd
    this compiled with -std=c++17

--- 0515/iterator_category.cpp
+++ 0515/iterator_category.cpp
@@ -3,7 +3,9 @@
 template<typename tag, typename Iterator>
 constexpr bool is_category_of() {
   using iter_tag = typename std::iterator_traits<Iterator>::iterator_category;
-  return std::is_base_of<tag, iter_tag>();
+  // with -std=c++17 this compiles; without -std=c++17, this should be changed to
+  // return std::is_base_of<tag, iter_tag>();
+  return std::is_base_of_v<tag, iter_tag>;
 }
 
 int main(){
