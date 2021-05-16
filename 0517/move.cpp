48dc605df0188642bdecca058586215baaddf30c
Ryo Kajiwara <sylph01@s01.ninja>
2021-05-16 23:39:04 +0900

    [0517] ezoe-book: annotate

--- 0517/move.cpp
+++ 0517/move.cpp
@@ -2,6 +2,9 @@
 
 template <typename T>
 std::remove_reference_t<T> && move(T && t) noexcept {
+  // if an lvalue of type U is passed, template parameter T would be U &
+  // then the && would be ignored, returning a type of U &, which does not match
+  // so, the reference needs to be removed in order to apply && to U
   return static_cast<std::remove_reference_t<T> &&>(t);
 }
 
@@ -9,6 +12,6 @@ int main(){
   int lvalue {};
   // passing lvalue
   int && r =  move(lvalue);
-  // passing rvalue
+  // passing prvalue
   int && r2 = move(1 + 1);
 }
